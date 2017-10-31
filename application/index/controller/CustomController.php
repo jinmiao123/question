c0d67e96001760e0e82134f7102d99da1f28bd11
jinmiao <1872310766@qq.com>
2017-10-31 16:02:39 +0800

    project init

---
+++ application/index/controller/CustomController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jinmiao
+ * Date: 2017/10/31
+ * Time: 上午11:31
+ */
+namespace app\index\controller;
+use think\controller;
+
+class CustomController extends controller
+{
+
+}
---
+++ application/index/model/Customs.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jinmiao
+ * Date: 2017/10/31
+ * Time: 上午11:28
+ */
+namespace app\index\model;
+use think\Model;
+
+class Customs extends Model
+{
+
+}
